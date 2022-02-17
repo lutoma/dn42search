599df7f5efa690f86a8129273f7280eb7c66c0e1
Lukas Martini <hello@lutoma.org>
2022-02-17 21:19:57 +0100

    API: Add basic pagination

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from math import ceil
 import requests
 
 app = FastAPI()
@@ -16,10 +18,12 @@ def read_root():
 
 
 @app.get("/search/")
-def read_item(q: str):
+def read_search(q: str, page: Optional[int] = 1):
 		http_response = requests.get('http://search.dn42:8983/solr/dn42search/select', params={
 			'q': q,
-			'fl': 'id,title,excerpt,url,size,last_indexed,mime'
+			'fl': 'id,title,excerpt,url,size,last_indexed,mime',
+			'rows': 15,
+			'start': (page - 1) * 15
 		})
 
 		data = http_response.json()
@@ -28,4 +32,9 @@ def read_item(q: str):
 			return {'status': 'error', 'error': data['error']['msg']}
 
 		response = data['response']
-		return {'status': 'ok', 'count': response['numFound'], 'results': response['docs']}
+		return {
+			'status': 'ok',
+			'count': response['numFound'],
+			'pages': ceil(response['numFound'] / 15),
+			'results': response['docs']
+		}
