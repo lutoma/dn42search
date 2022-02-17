442f59c08faee13f62807f783e377e1134e58bc2
Lukas Martini <hello@lutoma.org>
2022-02-17 15:57:31 +0100

    Update frontend API path for production

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   publicRuntimeConfig: {
-    API_BASE: 'http://localhost:8000'
+    API_BASE: 'https://api.search.dn42'
   }
 })
