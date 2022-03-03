494c9388863b533f053eaa0b4437cd754bb23e2c
Lukas Martini <hello@lutoma.org>
2022-03-03 07:32:49 +0100

    frontend: Use API on same domain to fix HTTPs issues

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   publicRuntimeConfig: {
-    API_BASE: 'https://api.search.dn42'
+    API_BASE: '/iqa'
   }
 })
